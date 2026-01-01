237265509fc7ccc96c930230cef128d5037dc511
mayabarlev10-hub <maya.barlev10@gmail.com>
2026-01-02 01:03:53 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,7 +1,7 @@
-from PIL import Image
-import numpy as np
-from scipy.signal import convolve2d
-import matplotlib.pyplot as plt
+from PIL import Image # To read images
+import numpy as np # To convert image to NumPy array
+import matplotlib.pyplot as plt # To plot the image
+
 
 def load_image(path):
     img = Image.open(path)
